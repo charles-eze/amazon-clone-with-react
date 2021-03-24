20765598d8bf694d7ce4a735f82bffa74e605c1f
charles-eze <charleseze.github.com>
2021-03-24 02:55:19 +0100

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyA_D7K6L6II32o-aw6-aimB01-d6F0DTNM",
   authDomain: "clone-d9b8d.firebaseapp.com",
@@ -8,3 +10,9 @@ const firebaseConfig = {
   appId: "1:151650550634:web:a9600668557295544b1b13",
   measurementId: "G-6EQCB8VJZK"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
