ec01b665fcf38592d45d7ce281a603479ef2a143
charles-eze <charleseze.github.com>
2021-03-29 13:07:21 +0100

    clean commit 2

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/clone-d9b8d/us-central1/api' //This is the cloud function API url
+    baseURL: 'http://localhost:5001/clone-d9b8d/us-central1/api' //This is the cloud function API url for localhost
 });
 
 export default instance;
